c6a58a1e0a56a085d329f2fc8f5d39750498edbf
Andrew <drewrockz01@gmail.com>
2022-05-11 22:06:49 +1000

    Show textbox to edit field when clicking rename

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,21 +4,20 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
-var lastClickedElement = '';
+
+let lastClickedElement = '';
 
 $(document).on('contextmenu', 'li', function(e) {
     e.preventDefault();
-    lastClickedElement = this.id
+    lastClickedElement = this.id;
     window.contextBridgeApi.send('show-context-menu', e.originalEvent);
 });
 
-// window.addEventListener('contextmenu', (e) => {
-//     e.preventDefault()
-//     window.contextBridgeApi.send('show-context-menu', e);
-// });
-
 window.contextBridgeApi.receive('context-menu-command', (event, command) => {
     if (command == 'menu-item-1') {
-        console.log(this.lastClickedElement);
+        console.log(lastClickedElement);
+        const text = $('#' + lastClickedElement + ' span').text();
+        const renameHtml = '<input type="text" id="name" value="' + text + '"required minlength="4" maxlength="8" size="10">';
+        $('#' + lastClickedElement + ' span').replaceWith(renameHtml);
     }
 });
